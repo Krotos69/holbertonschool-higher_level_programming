8ce4bc6c694957a32e94886ccb6dc1947be5d908
eugenio martinez <emartinez714@gmail.com>
2026-01-18 22:38:47 -0400

    fix whitespaces

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     result = ""
     for char in str:
-        if ord(char) >=97 and ord(char) <= 122:
+        if ord(char) >= 97 and ord(char) <= 122:
             result += chr(ord(char) -32)
         else:
             result = result + char
