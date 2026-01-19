932b16aaf9753ec94761f4a79c1ef31526e27a3c
eugenio martinez <emartinez714@gmail.com>
2026-01-18 21:27:50 -0400

    delete ##

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-###prints all possible different combinations of two digits
+# prints all possible different combinations of two digits
 for first_digit in range(0, 10):
     for second in range(first_digit + 1, 10):
         if first_digit == 8 and second == 9:
