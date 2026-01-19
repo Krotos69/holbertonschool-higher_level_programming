fcc41daf8c8037261af8b7027af26cd5da9ecfc2
eugenio martinez <emartinez714@gmail.com>
2026-01-18 22:35:15 -0400

    fix spaces

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # function that prints a string in uppercase followed by a new line
 def uppercase(str):
-	result = ""
-	for char in str:
-		if ord(char) >=97 and ord(char) <= 122:
-			result += chr(ord(char) -32)
-		else:
-			result = result + char
-	print("{}".format(result))
+    result = ""
+    for char in str:
+        if ord(char) >=97 and ord(char) <= 122:
+            result += chr(ord(char) -32)
+        else:
+            result = result + char
+    print("{}".format(result))
