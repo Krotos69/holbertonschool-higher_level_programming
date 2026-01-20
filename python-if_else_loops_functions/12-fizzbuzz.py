df5445fd45a1e8e066a42044cc1585152e7f4890
eugenio martinez <emartinez714@gmail.com>
2026-01-20 17:47:37 -0400

    FizzBuzz prints the numbers from 1 to 100 separated by a space

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    # Imprime los números del 1 al 100
+	for number in range(1, 101):
+		# Comprueba si el número es divisible por 3 y 5
+		if number % 3 == 0 and number % 5 == 0:
+			print("FizzBuzz", end=" ")
+		# Comprueba si el número es divisible por 3
+		elif number % 3 == 0:
+			print("Fizz", end=" ")
+			# Comprueba si el número es divisible por 5
+		elif number % 5 == 0:
+			print("Buzz", end=" ")
+		else:
+			print(number, end=" ")
+# Llamar a la función fizzbuzz
+fizzbuzz()
