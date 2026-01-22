4478603718b6ff6c0772a7a1837a876b6e7f2aac
eugenio martinez <emartinez714@gmail.com>
2026-01-22 17:33:36 -0400

    spaces correction & indentations

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
-    # calcular len_args
-    len_args = len(argv) -1
-    if len_args == 0:
-    # imprimir "0 arguments."
+    len_args = len(argv) -1 # calcular len_args
+
+    if len_args == 0: # imprimir "0 arguments."
         print("0 arguments.")
-    elif len_args == 1:
-    # imprimir "1 argument:"
+    elif len_args == 1: # imprimir "1 argument:"
         print("1 argument:")
     else:
-    # imprimir "<len_args> arguments:"
-        print(f"{len_args} arguments:")
-for i in range(1, len_args + 1):
-    # imprimir "<i>: <argv[i]>"
-    print(f"{i}: {argv[i]}")
+        print(f"{len_args} arguments:") # imprimir "<len_args> arguments:"
+
+    for i in range(1, len_args + 1): # imprimir "<i>: <argv[i]>"
+        print(f"{i}: {argv[i]}")
