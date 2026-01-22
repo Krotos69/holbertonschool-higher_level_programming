94c6369b09659c8533df4cd3f2bf76ff0067e97f
eugenio martinez <emartinez714@gmail.com>
2026-01-22 17:48:22 -0400

    eliminate comments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 from sys import argv
 
 if __name__ == "__main__":
-    len_args = len(argv) -1 # calcular len_args
+    len_args = len(argv) -1
 
-    if len_args == 0: # imprimir "0 arguments."
+    if len_args == 0:
         print("0 arguments.")
-    elif len_args == 1: # imprimir "1 argument:"
+    elif len_args == 1:
         print("1 argument:")
     else:
-        print(f"{len_args} arguments:") # imprimir "<len_args> arguments:"
+        print(f"{len_args} arguments:")
 
-    for i in range(1, len_args + 1): # imprimir "<i>: <argv[i]>"
+    for i in range(1, len_args + 1):
         print(f"{i}: {argv[i]}")
