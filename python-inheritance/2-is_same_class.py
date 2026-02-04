32f074d153fda06725bc7913ff24b55deded6bd2
eugenio martinez <emartinez714@gmail.com>
2026-02-04 18:53:27 -0400

    docstring fix and add 2 blanc line

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Module that defines is_same_class funtion. """
+
+
 def is_same_class(obj, a_class):
-    """ Function that returns True if the object is exactly an instance of the specified class ; otherwise False.
+    """ Function that returns True if the object is exactly an instance of 
+    the specified class ; otherwise False.
     Args:
         obj: The object to be checked.
         a_class: The class to be compared against.
