db44627067afd635c1b4985326abbfa884361eaf
eugenio martinez <emartinez714@gmail.com>
2026-02-05 18:33:09 -0400

    fix the length and indetation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,13 +3,13 @@
 
 
 def inherits_from(obj, a_class):
-    """ Function that returns True if the object is an instance of a class 
-    that inherited (directly or indirectly) from the specified class ; 
+    """ Function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class ;
     otherwise False.
     Args:
         obj: The object to be checked.
         a_class: The class to be compared against.
     Returns:
         True if obj is an instance of a class that inherited from a_class,
-		False otherwise. """
+        False otherwise. """
     return issubclass(type(obj), a_class) and type(obj) != a_class
