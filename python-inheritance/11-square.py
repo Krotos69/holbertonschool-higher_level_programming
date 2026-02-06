731140a779d4471aae1780634ec45fbec720f70d
eugenio martinez <emartinez714@gmail.com>
2026-02-06 16:39:38 -0400

    Square2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module that defines Square class, which inherits from Rectangle. """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that inherits from Rectangle and defines a square
+    with size. """
+
+    def __init__(self, size):
+        """ Validates using inherited integer_validator and initializes
+        size as both width and height."""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Returns area of the square.	"""
+        return super().area()
+
+    def __str__(self):
+        """ Returns a string representation of the square.
+
+        Returns:
+            str: A string in the format [Square] <size>/<size>.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
