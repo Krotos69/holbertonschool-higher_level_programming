890ba3936bcbf42b4905f86caa170dd1c9181a34
eugenio martinez <emartinez714@gmail.com>
2026-02-14 18:43:42 -0400

    function that returns an object Python data structure represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that returns an object (Python data structure) represented by a JSON string:
+"""
+import json
+
+def from_json_string(my_str):
+    """
+    Return the object represent by json string
+    """
+    return json.loads(my_str)
