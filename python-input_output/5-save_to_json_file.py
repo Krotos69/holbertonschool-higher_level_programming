4c6971484e96677ec64dba6b75e185a73322cb45
eugenio martinez <emartinez714@gmail.com>
2026-02-14 19:34:47 -0400

    fix docstring error and new line

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Function that writes an Object to a text file, using a JSON representation:"
+Function that writes an Object to a text file, using a JSON representation:
 """
 import json
+
+
 """ save object in json"""
 def save_to_json_file(my_obj, filename):
     with open(filename, 'w') as f:
