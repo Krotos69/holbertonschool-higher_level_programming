c3dbda826042c7737fadd84cc47754d1d55c9722
eugenio martinez <emartinez714@gmail.com>
2026-02-14 19:30:43 -0400

    fixline too long and newl ine

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-function that returns an object (Python data structure) represented by a JSON string:
+Returns an object (Python data structure) represented by a JSON string:
 """
 import json
+
 
 def from_json_string(my_str):
     """
