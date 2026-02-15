42497636dfbac1c03afd64facae4be48120072e7
eugenio martinez <emartinez714@gmail.com>
2026-02-15 14:02:24 -0400

    fix blank line

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ returns the dictionary description with simple data structure """
 
+
 def class_to_json(obj):
     """ Returns dictionary representation of object """
     return obj.__dict__
