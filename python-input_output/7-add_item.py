0a1207554685b0b95b4eea6ffc6543224c21112f
eugenio martinez <emartinez714@gmail.com>
2026-02-15 11:49:59 -0400

    fix imports cannot recognice .py file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,15 +2,14 @@
 """ adds all arguments to a Python list, and then save them to a file """
 
 import sys
-import os
-
-from save_to_json_file.py import save_to_json_file
-from load_from_json_file.py import load_from_json_file
+from os.path import exists
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
 #Load existing list or create a new one
-if exists?(filename):
+if exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
