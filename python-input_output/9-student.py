5b15f060846bdacbdaaaffb2835ea9fa85969b76
eugenio martinez <emartinez714@gmail.com>
2026-02-15 14:53:36 -0400

    fix blank line space

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,8 +3,9 @@
 class Student that defines a student
 """
 
+
 class Student:
-    
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
