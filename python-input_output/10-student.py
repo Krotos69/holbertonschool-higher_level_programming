f0105fc0a193a4b02539755778b155ca837bfd9a
eugenio martinez <emartinez714@gmail.com>
2026-02-15 15:25:19 -0400

    class Student that defines a student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class Student that defines a student by"""
+
+
+class Student:
+    """define student """
+    
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation in Student instance"""
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__
