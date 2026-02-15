671ef87ba33b14a313723d610433c21fa91fdb87
eugenio martinez <emartinez714@gmail.com>
2026-02-15 11:40:04 -0400

    fix error imports module filename rename files

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" script that adds all arguments to a Python list, and then save them to 
-    a file """
-    
+""" adds all arguments to a Python list, and then save them to a file """
+
 import sys
 import os
 
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_from_json_file
+from save_to_json_file.py import save_to_json_file
+from load_from_json_file.py import load_from_json_file
 
 filename = "add_item.json"
 
+#Load existing list or create a new one
 if exists?(filename):
     items = load_from_json_file(filename)
 else:
