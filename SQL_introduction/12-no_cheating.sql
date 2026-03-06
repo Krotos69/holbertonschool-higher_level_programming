47ccc48435e01d8a1f96bd7dc4f9828a8f94d590
eugenio martinez <emartinez714@gmail.com>
2026-03-05 20:56:33 -0400

    FIX UPPERCASE LETTER

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
--- Command script that updates the score of Bob to 10 in the table second_table
-update second_table
+-- Command that updates the score of Bob to 10 in the table second_table
+UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
