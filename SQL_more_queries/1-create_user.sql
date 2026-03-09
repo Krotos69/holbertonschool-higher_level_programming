732ea0fe77640225df51b7541de2f764f55e1747
eugenio martinez <emartinez714@gmail.com>
2026-03-09 15:43:00 -0400

    fix note

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Command  that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all privileges on the entire MySQL server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
