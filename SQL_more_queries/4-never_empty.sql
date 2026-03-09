189ce187a13b787133eec9b2b2989c42e1e53a3e
eugenio martinez <emartinez714@gmail.com>
2026-03-09 18:53:49 -0400

    change code to add value

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Command that creates the table id_not_null if it does not already exist
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT value 1,
+	id INT NOT NULL DEFAULT 1,
 	name VARCHAR(256)
 );
