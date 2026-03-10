bd16af3900199454358836b14373e3e63c384474
eugenio martinez <emartinez714@gmail.com>
2026-03-09 23:37:42 -0400

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Command that database if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Switch to the database
+USE hbtn_0d_usa;
+-- Create the table if it does not already exist
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256)
+);
