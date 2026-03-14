893a7c3423d9844de4f6e6cc3f6014399572e2fc
eugenio martinez <emartinez714@gmail.com>
2026-03-14 18:38:23 -0400

    fix leng and space

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,11 +3,14 @@
 
 Prototype: def safe_print_division(a, b):
 You can assume that a and b are integers
-The result of the division should print on the finally: section preceded by Inside result:
+The result of the division should print on the finally:
+section preceded by Inside result
 Returns the value of the division, otherwise: None
 You have to use try: / except: / finally:
 You have to use "{}".format() to print the result
-You are not allowed to import any module """
+You are not allowed to import any module 
+"""
+
 
 def safe_print_division(a, b):
     try:
