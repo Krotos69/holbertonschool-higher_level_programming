073aa78a7ac4d02bb5e5654d424c80b666e47ced
eugenio martinez <emartinez714@gmail.com>
2026-03-15 16:32:00 -0400

    fix whitespace

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -8,11 +8,11 @@ class Square:
     def __init__(self, size=0):
         """Initialize the square with a private size attribute."""
         self.size = size
-        
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -24,7 +24,7 @@ class Square:
     def area(self):
         """Return the area of the square."""
         return self.__size * self.__size
-    
+
     def my_print(self):
         if self.__size == 0:
             print("")
