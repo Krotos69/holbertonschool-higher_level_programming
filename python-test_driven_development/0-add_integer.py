5f5186fd9b0f575ffe2c6748dcd4a441567cd82b
eugenio martinez <emartinez714@gmail.com>
2026-03-15 19:36:24 -0400

    add missing docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" this module provides a function 'add_interger' that adds two intergers"""
+
+
 def add_integer(a, b=98):
     """ adds two integers or floats after casting to int."""
     if not isinstance(a, (int,float)):
