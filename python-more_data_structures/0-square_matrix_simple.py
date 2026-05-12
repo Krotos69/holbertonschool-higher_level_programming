f635016f9f969d9ad00f8dd3ac388a8fd3a10790
eugenio martinez <emartinez714@gmail.com>
2026-05-12 17:03:04 -0400

    missing whitespace around operator

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     """computes the square value of all intergers of a matrix"""
     new_matrix = []
     for row in matrix:
-        new_row =list(map(lambda x: x ** 2, row))
+        new_row = list(map(lambda x: x ** 2, row))
         new_matrix.append(new_row)
     return new_matrix
