c05d8b9ccfa7d742364a4712577929877ce84461
eugenio martinez <emartinez714@gmail.com>
2026-05-12 20:51:35 -0400

    fix code

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """search and replace all occurrences of an element by another in a new line list"""
-    new_list =[]
+    """Search and replace all occurrences of an element in a new list."""
+    new_list = []
     for element in my_list:
         if element == search:
             new_list.append(replace)
