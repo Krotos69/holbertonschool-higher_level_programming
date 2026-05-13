0a852cdd50555a78aca1b2bb3c584fe0252b89ca
eugenio martinez <emartinez714@gmail.com>
2026-05-13 19:24:52 -0400

    fix note

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
+"""
+    Print the first x elements of a list that are integers.
+    Return the number of integers printed.
+"""
+
+
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    Prints the first x elements of a list that are integers.
-    Returns the number of integers printed.
-    """
+
     count = 0
-    try:
-        for i in range(x):
-            try:
-                # We use a temporary variable to hold the formatted string
-                # so we can print them on the same line.
-                # Since we must print them on the same line,
-                # we use end=' ' or similar, but the prompt says
-                # "on the same line followed by a new line".
-                    # The most robust way is to use print(..., end=' ')
-                    # and then a final print() for the new line.
-                print("{:d}".format(my_list[i]), end=' ')
-                count += 1
-            except (ValueError, TypeError):
-                continue
-    except IndexError:
-        pass
-    print("") # Print the final new line
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print()
     return count
