ead849e1f956c1ae2d246f392926fb4cba12a504
eugenio martinez <emartinez714@gmail.com>
2026-05-13 19:40:37 -0400

    add Exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines a function that prints an integer with "{:d}".format()."""
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except Exception:
         return False
