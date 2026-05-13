cfbdde5388a8a882b0d9cdb566d07a5af803a60c
eugenio martinez <emartinez714@gmail.com>
2026-05-13 19:59:35 -0400

    function that raises a name exception with a message.

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""WWrite a function that raises a name exception with a message.
+
+Prototype: def raise_exception_msg(message=""):
+You are not allowed to import any module
+"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
