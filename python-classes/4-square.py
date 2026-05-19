ff554b5824deadcbcf8320fca039ee0966b0b99a
eugenio martinez <emartinez714@gmail.com>
2026-05-19 19:12:59 -0400

    fix code

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-""" Define a square class"""
+""" Define a Square class"""
 
 
 class Square:
+    """Defines Area of a square"""
+
     def __init__(self, size=0):
-        self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+        """Initialize the square with a private size attribute."""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
     def area(self):
+        """Return the area of the square."""
         return self.__size * self.__size
