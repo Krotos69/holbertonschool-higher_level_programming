c95c6b49ee205133c6a44f0d6079a02e8af28823
eugenio martinez <emartinez714@gmail.com>
2026-05-19 16:11:54 -0400

    fix notes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,6 @@
 
 
 class Square: # Define a class named Square
-	"""A class that defines a square.""" 
-    def __init__(self, size):
-        """init method to initialize the size of the square."""
+	"""A class that defines a square""" 
+    def __init__(self, size): # init method to initialize the size of the square.
         self.__size = size # Private instance attribute to store the size of the square
