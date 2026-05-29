7c939be0218447b2112a825ece59a895ffb454b1
eugenio martinez <emartinez714@gmail.com>
2026-05-29 14:27:18 -0400

    Add inherits_from function to check subclass-based instance relationships

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a function is_same_class."""
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; otherwise False """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
