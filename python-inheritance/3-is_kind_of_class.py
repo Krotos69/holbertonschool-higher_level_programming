22253cf8a7bae53ca81fbe5088fd0c99b851cf3f
eugenio martinez <emartinez714@gmail.com>
2026-05-29 14:36:44 -0400

    fix long

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """ Returns True if the objects is an isntance of a class that inheritance from the specified class; otherwise False."""
+    """ Returns True if the objects is an isntance of a class
+    that inheritance from the specified class; otherwise False."""
     return isinstance(obj, a_class)
