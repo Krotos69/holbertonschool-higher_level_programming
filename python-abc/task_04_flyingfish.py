6ddfe0536a1a6a1346b0ff9d897c08cc383bdb78
eugenio martinez <emartinez714@gmail.com>
2026-05-31 20:37:01 -0400

    save code

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Fish:
+	"""A class representing a fish."""
+
+	def swim(self):
+		print("The fish is swimming.")
+
+	def habitat(self):
+		print("The fish lives in water.")
+
+class Bird:
+	"""A class representing a bird."""
+
+	def fly(self):
+		print("The bird is flying.")
+
+	def habitat(self):
+		print("The bird lives in the sky.")
+
+class FlyingFish(Fish, Bird):
+	"""A class representing a flying fish, which can both swim and fly."""
+
+	def fly(self):
+		print("The flying fish is soaring!")
+		
+	def habitat(self):
+		print("The flying fish lives both in water and the sky!")
+
+	def swim(self):
+		print("The flying fish is swimming!")
