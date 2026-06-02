c45c82fe985027e0f99ca6ff43dbf08dd9bc365f
eugenio martinez <emartinez714@gmail.com>
2026-06-02 18:51:29 -0400

    fix space

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Defines a function that reads a text file and prints it to stdout."""
+"""Defines a function that reads a text file and prints it to
+    stdout."""
+
 
 def read_file(filename=""):
     """Reads a text file and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+        Defaults to an empty string.
     """
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
