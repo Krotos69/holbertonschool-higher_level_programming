c34a11c1ee7bb3daa3f46ca58c17ccd53a7f1a52
eugenio martinez <emartinez714@gmail.com>
2026-06-02 20:51:49 -0400

    fix line 8

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 def write_file(filename="", text=""):
     """Write a string to a text file and return the characters written."""
 
-    with open(filename, "w", encoding="utf-8") as file: 
+    with open(filename, "w", encoding="utf-8") as file:
         # Open the file in write mode with UTF-8 encoding
         return file.write(text)
