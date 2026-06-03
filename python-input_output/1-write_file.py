08f7c91637cb77d97547f00d424941cdbd5e9642
eugenio martinez <emartinez714@gmail.com>
2026-06-02 20:43:45 -0400

    fix line long

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,8 +5,7 @@
 def write_file(filename="", text=""):
     """Write a string to a text file and return the characters written."""
 
-
     with open(filename, "w", encoding="utf-8") as file: 
         # Open the file in write mode with UTF-8 encoding
-        return file.write(text) 
-    # Write the text to the file and return the number of characters written
+        return file.write(text)
+        #Write the text to the file and return the number of characters written
