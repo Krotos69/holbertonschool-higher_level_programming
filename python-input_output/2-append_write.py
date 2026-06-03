219fae2027d61a3f6731bd139d7932b67f3f102a
eugenio martinez <emartinez714@gmail.com>
2026-06-02 21:06:18 -0400

    fix 2 blankline

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """define a function that appends text to a file"""
+
+
 def append_write(filename="", text=""):
-    """Append a string to text file and return the number of characters added"""
+    """Append a string to text file and return the number of cha added"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
