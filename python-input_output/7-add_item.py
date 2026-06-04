c1b4c28689fbac78faba7ad0be412b065188d4d4
eugenio martinez <emartinez714@gmail.com>
2026-06-03 23:10:46 -0400

    Load, add, save Write a script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a function that adds all arguments to a Python list, and then save them to a file."""
+
+import sys
+from os.path import exists
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+# Load existing list or create a new one
+if exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+# Add all command-line arguments except the script name
+items.extend(sys.argv[1:])
+
+# Save the updated list
+save_to_json_file(items, filename)
