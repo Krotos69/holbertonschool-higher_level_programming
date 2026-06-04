81184b1b33e05b25bef4d4314e63e37c5f4147fc
eugenio martinez <emartinez714@gmail.com>
2026-06-03 23:57:49 -0400

    fix the attribute to_json and identations

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """ Write a class Student that defines a student"""
 
+
 class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        def  to_json(self, attrs=None):
-            if type(attrs) is list and all(type(i) is str for i in attrs):
-                new_dict = {}
-                for i in attrs:
-                    if i in self.__dict__:
-                        new_dict[i] =self.__dict__[i]
-                        return new_dict
-            return self.__dict__
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(type(a) is str for a in attrs):
+            return {key:getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
