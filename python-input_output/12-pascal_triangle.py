b294369ce818e53d461a7a1c70909ee10c546c9f
eugenio martinez <emartinez714@gmail.com>
2026-06-04 11:43:32 -0400

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" """
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # first row
+
+    for i in range(1, n):
+        prev = triangle[-1]
+        row = [1]  # first element
+
+        # middle elements
+        for j in range(1, len(prev)):
+            row.append(prev[j - 1] + prev[j])
+
+        row.append(1)  # last element
+        triangle.append(row)
+
+    return triangle
