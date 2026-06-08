95719cb0aba37c9e6b079f4ae53cd287dbd68d1c
eugenio martinez <emartinez714@gmail.com>
2026-06-07 22:31:25 -0400

    correct the imports

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,9 @@
 
 import sys
 from os.path import exists
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 # Define the filename to store the list
 filename = "add_item.json"
